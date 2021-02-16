f5dd8377d7aff7b7f18e482257cb32d047f077f1
xiaoma521 <1273114582@qq.com>
2021-02-16 01:59:31 -0500

    final

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,25 @@
 /* Put your javascript in here */
+
+const carousel = document.getElementById("carousel");
+let position = 0;
+let width = 130; // image width
+let count = 4; // visible images count
+
+function moveCarousel(event) {
+  if (event.target.classList.contains("arrow_right")) {
+    position = position - width * count;
+    event.currentTarget.querySelector(
+      ".slidesImage"
+    ).style.transform = `translateX(${position}px)`;
+  }
+  if (event.target.classList.contains("arrow_left")) {
+    position = position + width * count;
+    event.currentTarget.querySelector(
+      ".slidesImage"
+    ).style.transform = `translateX(${position}px)`;
+  }
+
+  console.log(position);
+}
+
+carousel.addEventListener("click", moveCarousel);
